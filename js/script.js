e50d423c6515585001b07c105eed0860c119bb43
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-06-01 16:52:20 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+window.onload = function() {
+  const params = new URLSearchParams(document.location.search)
+
+  //input
+  const radius = params.get('radius')
+  console.log(radius)
+
+  //process
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const dimensions = "<ul>\n<li>a base = " + volume.toFixed(2)
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm²'
 }
