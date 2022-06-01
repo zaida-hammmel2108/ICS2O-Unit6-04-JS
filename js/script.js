eba97f72d5fbf2e2a5283b5c808162c1b9bc6d4f
zaida-hammel <zaida-hammel@users.noreply.github.com>
2022-06-01 16:54:41 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-window.onload = function() {
-  const params = new URLSearchParams(document.location.search)
+window.onload = function () {
+  const params = new URLSearchParams(document.location.search);
 
   //input
-  const radius = params.get('radius')
-  console.log(radius)
+  const radius = params.get("radius");
+  console.log(radius);
 
   //process
-  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
-  const dimensions = "<ul>\n<li>a base = " + volume.toFixed(2)
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  const dimensions = "<ul>\n<li>a base = " + volume.toFixed(2);
 
   // output
-  document.getElementById('dimensions').innerHTML = dimensions
-  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm²'
-}
+  document.getElementById("dimensions").innerHTML = dimensions;
+  document.getElementById("volume").innerHTML = "Volume is: " + volume + " mm²";
+};
